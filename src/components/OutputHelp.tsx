8d31b5f089fa05f93d24ac13c00456267ce86a5f
Benjamin Krall <benjaminkrall@gmail.com>
2021-08-07 22:09:33 -0400

    more fixes

--- src/components/OutputHelp.tsx
+++ src/components/OutputHelp.tsx
@@ -7,25 +7,21 @@ const OutputHelp = () => {
 
   return (
     <div className="border-2 border-dashed border-yellow-300 bg-yellow-300 bg-opacity-10 p-2">
-      <dl>
-        <dt>Welcome to the krall.dev terminal. Run any of the following commands to find learn more.</dt>
+      <p>Welcome to the krall.dev terminal. Run any of the following commands to find learn more.</p>
 
-        <dd className="mt-2 ml-4">
-          <dl>
-            {Object.entries(commands).map(([key, command]) =>
-              command.helpLabel ? (
-                <>
-                  <dt className="text-blue-400">
-                    <button onClick={() => setTerminalValue(key)} className="hover:underline">
-                      {command.helpLabel}
-                    </button>
-                  </dt>
-                  <dd className="ml-4 mb-2">{command.helpDescription}</dd>
-                </>
-              ) : null
-            )}
-          </dl>
-        </dd>
+      <dl className="ml-4 mt-2">
+        {Object.entries(commands).map(([key, command]) =>
+          command.helpLabel ? (
+            <>
+              <dt className="text-blue-400">
+                <button onClick={() => setTerminalValue(key)} className="hover:underline">
+                  {command.helpLabel}
+                </button>
+              </dt>
+              <dd className="ml-4 mb-2">{command.helpDescription}</dd>
+            </>
+          ) : null
+        )}
       </dl>
     </div>
   )
